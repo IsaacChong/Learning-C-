b02764fba9ead571ebe9b610033ed84f9d1042bb
Isaac-websparks <isaac.chong@websparks.sg>
2023-05-03 10:39:36 +0800

    Part 6

--- Program.cs
+++ Program.cs
@@ -1,23 +1,42 @@
-﻿string permission = "Admin|Manager";
-int level = 55;
+﻿string ip = "1.1.1.1";
 
-if (permission.Contains("Admin") && level > 55)
+TestIp(ip);
+
+void TestIp(string address)
 {
-    Console.WriteLine("Welcome, Super Admin user.");
-}
-else if (permission.Contains("Admin") && level <= 55)
-{
-    Console.WriteLine("Welcome, Admin user.");
-}
-else if (permission.Contains("Manager") && level >= 20 && !permission.Contains("Admin"))
-{
-    Console.WriteLine("Contact an Admin for access.");
-}
-else if (permission.Contains("Manager") && level < 20 && !permission.Contains("Admin"))
-{
-    Console.WriteLine("You do not have sufficient privileges.");
-}
-else if (!permission.Contains("Manager") && !permission.Contains("Admin"))
-{
-    Console.WriteLine("You do not have sufficient privileges.");
+    string[] ipArray = address.Split(".");
+    if (ipArray.Length == 4)
+    {
+        Boolean leadingZero = false;
+        foreach (string item in ipArray)
+        {
+            if (item.Substring(0, 1) == "0")
+            {
+                leadingZero = true;
+                break;
+            }
+        }
+        if (!leadingZero)
+        {
+            Boolean correctNumRange = true;
+            foreach (string item in ipArray)
+            {
+                int numItem = int.Parse(item);
+                if (numItem < 0 || numItem > 255) {
+                    correctNumRange = false;
+                    break;
+                }
+
+            }
+            if (!!correctNumRange) {
+                Console.WriteLine("IP address is valid");
+            } else {
+                Console.WriteLine("Number in the IP address is either less than 0 or more than 255, IP is invalid");
+            }
+        } else {
+            Console.WriteLine("There are leading zeros in the IP address numbers, IP is invalid");
+        }
+    } else {
+        Console.WriteLine("IP address length is shorter than 4 or longer than 4, IP is invalid");
+    }
 }
